58b0be70d7cb01b00e60148b6ab9e73bbccbf682
luxidea <contact@luxidea.net>
2020-02-05 08:13:56 +0900

    no message

--- TreeNode.dpr
+++ TreeNode.dpr
@@ -5,6 +5,8 @@ uses
   FMX.Forms,
   Main in 'Main.pas' {Form1},
   Core in 'Core.pas',
+  LUX.Data.Tree.core in '_LIBRARY\LUXOPHIA\LUX\Data\Tree\LUX.Data.Tree.core.pas',
+  LUX.Data.Tree.Node in '_LIBRARY\LUXOPHIA\LUX\Data\Tree\LUX.Data.Tree.Node.pas',
   LUX.Data.Tree in '_LIBRARY\LUXOPHIA\LUX\Data\Tree\LUX.Data.Tree.pas';
 
 {$R *.res}
